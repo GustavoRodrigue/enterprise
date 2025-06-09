bf57cea0a7973a17c0646d96ad477b429ca5f777
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2025-06-09 10:57:34 -0300

    Criando o schema de enterprise

--- src/enterprise/repositories/schemas/enterprise.schema.ts
+++ src/enterprise/repositories/schemas/enterprise.schema.ts
@@ -4,7 +4,45 @@ import { HydratedDocument } from "mongoose";
 
 export class EnterpriseSchema{
     
-   
+    @Prop({type: String, required: true})
+    companyName: string;
+    @Prop({type: String, required: true})
+    fantasyName: string;
+    @Prop({type: String, required: true})
+    email: string;
+    @Prop({type: String, required: true})
+    cnpj: string;
+    @Prop({type: String, required: true})
+    telephone: string;
+    @Prop({type: String, required: true})
+    descriptionService: string
+    @Prop({type: String, required: true})
+
+    nameResponsible: string
+    @Prop({type: String, required: true})
+    surnameResponsible: string;
+    @Prop({type: String, required: true})
+    emailResponsible: string;
+    @Prop({type: String, required: true})
+    telephoneResponsible: string;
+    @Prop({type: String, required: true})
+    
+    zipCode: string;
+    @Prop({type: String, required: true})
+    country: string;
+    @Prop({type: String, required: true})
+    city: string;
+    @Prop({type: String, required: true})
+    state: string;
+    @Prop({type: String, required: true})
+    district: string;
+    @Prop({type: String, required: true})
+    road: string;
+    @Prop({type: String, required: true})
+    propertyNumber: number;
+    @Prop({type: String, required: true})
+    complement: string;
 
 }
 
+export const EnterpriseSchemaFactory = SchemaFactory.createForClass(EnterpriseSchema);
