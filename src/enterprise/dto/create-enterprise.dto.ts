461bd2f00b94c4a3330422f4f7b5642759d4a3c6
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2025-06-09 11:03:29 -0300

    criando o Dto

--- src/enterprise/dto/create-enterprise.dto.ts
+++ src/enterprise/dto/create-enterprise.dto.ts
@@ -1,6 +1,41 @@
 import { IsEmail, IsNumber, IsOptional, IsString, IsStrongPassword } from "class-validator";
 
 export class CreateEnterpriseDto {
-   
+    @IsString()
+    companyName: string;
+        @IsString()
+        fantasyName: string;
+        @IsEmail()
+        email: string;
+        @IsString()
+        cnpj: string;
+        @IsString()
+        telephone: string;
+        @IsString()
+        descriptionService: string
+        @IsString()
+        nameResponsible: string
+        @IsString()
+        surnameResponsible: string;
+        @IsEmail()
+        emailResponsible: string;
+        @IsString()
+        telephoneResponsible: string;
+        @IsString()
+        zipCode: string;
+        @IsString()
+        country: string;
+        @IsString()
+        city: string;
+        @IsString()
+        state: string;
+        @IsString()
+        district: string;
+        @IsString()
+        road: string;
+        @IsNumber()
+        propertyNumber: number;
+        @IsString()
+        complement: string;
    
 }
